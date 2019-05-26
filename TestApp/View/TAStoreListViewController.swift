1acf5a7ee734371ca913109ef50b9561018002a8
Sunndararajan Krishnasamy <krish722@gmail.com>
2019-05-26 19:57:07 +1200

    Init project

---
+++ TestApp/View/TAStoreListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TAStoreListViewController.swift
+//  TestApp
+//
+//  Created by Krish on 26/05/19.
+//  Copyright © 2019 Plexure. All rights reserved.
+//
+
+import UIKit
+
+class TAStoreListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
